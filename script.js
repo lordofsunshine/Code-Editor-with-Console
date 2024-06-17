98ab6ca8e94abd6b1168291603a20c9b4269716b
lordofsunshine <94736527+lordofsunshine@users.noreply.github.com>
2024-06-17 20:15:46 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function run(){
+let htmlCode = document.getElementById("html-code").value;
+let cssCode = document.getElementById("css-code").value;
+let jsCode = document.getElementById("js-code").value;
+let output = document.getElementById("output");
+let consoleLog = document.getElementById("console-log");
+
+consoleLog.innerHTML = '';
+
+output.contentWindow.console.log = function(message) {
+    let date = new Date();
+    let timestamp = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+    let formattedMessage = timestamp + ' - ' + message + '<br>';
+    consoleLog.innerHTML += formattedMessage;
+};
+
+output.contentWindow.onerror = function(message, source, lineno, colno, error) {
+    let date = new Date();
+    let timestamp = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+    let errorMessage = '<br>' + timestamp + ' - Error: ' + message;
+    errorMessage += '<br>Source: ' + source;
+    errorMessage += '<br>Line: ' + lineno + ', Column: ' + colno;
+    errorMessage += '<br>Error object: ' + error + '<br>';
+    consoleLog.innerHTML += errorMessage;
+    return true;
+};
+
+output.contentDocument.body.innerHTML = htmlCode + "<style>"+ cssCode +"</style>";
+
+try {
+    output.contentWindow.eval(jsCode);
+} catch (e) {
+    let date = new Date();
+    let timestamp = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+    let runtimeErrorMessage = '<br>' + timestamp + ' - Runtime Error: ' + e.message + '<br>';
+    consoleLog.innerHTML += runtimeErrorMessage;
+}
+}
