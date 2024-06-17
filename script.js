a7b5bb8db9c8f302765c3d06a79acc4d727f93bd
lordofsunshine <94736527+lordofsunshine@users.noreply.github.com>
2024-06-17 20:24:00 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 function run(){
-let htmlCode = document.getElementById("html-code").value;
-let cssCode = document.getElementById("css-code").value;
-let jsCode = document.getElementById("js-code").value;
-let output = document.getElementById("output");
-let consoleLog = document.getElementById("console-log");
+    let htmlCode = document.getElementById("html-code").value;
+    let cssCode = "<style>body { color: white; } " + document.getElementById("css-code").value + "</style>";
+    let jsCode = document.getElementById("js-code").value;
+    let output = document.getElementById("output");
+    let consoleLog = document.getElementById("console-log");
 
-consoleLog.innerHTML = '';
+    consoleLog.innerHTML = '';
 
-output.contentWindow.console.log = function(message) {
-    let date = new Date();
-    let timestamp = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
-    let formattedMessage = timestamp + ' - ' + message + '<br>';
-    consoleLog.innerHTML += formattedMessage;
-};
+    output.contentWindow.console.log = function(message) {
+        let date = new Date();
+        let timestamp = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+        let formattedMessage = timestamp + ' - ' + message + '<br>';
+        consoleLog.innerHTML += formattedMessage;
+    };
 
-output.contentWindow.onerror = function(message, source, lineno, colno, error) {
-    let date = new Date();
-    let timestamp = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
-    let errorMessage = '<br>' + timestamp + ' - Error: ' + message;
-    errorMessage += '<br>Source: ' + source;
-    errorMessage += '<br>Line: ' + lineno + ', Column: ' + colno;
-    errorMessage += '<br>Error object: ' + error + '<br>';
-    consoleLog.innerHTML += errorMessage;
-    return true;
-};
+    output.contentWindow.onerror = function(message, source, lineno, colno, error) {
+        let date = new Date();
+        let timestamp = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+        let errorMessage = '<br>' + timestamp + ' - Error: ' + message;
+        errorMessage += '<br>Source: ' + source;
+        errorMessage += '<br>Line: ' + lineno + ', Column: ' + colno;
+        errorMessage += '<br>Error object: ' + error + '<br>';
+        consoleLog.innerHTML += errorMessage;
+        return true;
+    };
 
-output.contentDocument.body.innerHTML = htmlCode + "<style>"+ cssCode +"</style>";
+    output.contentDocument.body.innerHTML = htmlCode + cssCode;
 
-try {
-    output.contentWindow.eval(jsCode);
-} catch (e) {
-    let date = new Date();
-    let timestamp = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
-    let runtimeErrorMessage = '<br>' + timestamp + ' - Runtime Error: ' + e.message + '<br>';
-    consoleLog.innerHTML += runtimeErrorMessage;
-}
+    try {
+        output.contentWindow.eval(jsCode);
+    } catch (e) {
+        let date = new Date();
+        let timestamp = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+        let runtimeErrorMessage = '<br>' + timestamp + ' - Runtime Error: ' + e.message + '<br>';
+        consoleLog.innerHTML += runtimeErrorMessage;
+    }
 }
